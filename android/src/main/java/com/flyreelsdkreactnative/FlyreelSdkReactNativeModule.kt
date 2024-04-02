f0f8a265e8c7ebe2b8dcd3708e781f0e97d9fe43
Paweł Ługowski <pawel.lugowski@crustlab.com>
2024-04-02 15:15:03 +0200

    feat: added android interface

--- android/src/main/java/com/flyreelsdkreactnative/FlyreelSdkReactNativeModule.kt
+++ android/src/main/java/com/flyreelsdkreactnative/FlyreelSdkReactNativeModule.kt
@@ -1,11 +1,16 @@
 package com.flyreelsdkreactnative
 
+import android.app.Application
+import android.net.Uri
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.lexisnexis.risk.flyreel.Flyreel
+import com.lexisnexis.risk.flyreel.FlyreelConfiguration
+import com.lexisnexis.risk.flyreel.FlyreelEnvironment
 
-class FlyreelSdkReactNativeModule(reactContext: ReactApplicationContext) :
+class FlyreelSdkReactNativeModule(private val reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
 
   override fun getName(): String {
@@ -15,11 +20,58 @@ class FlyreelSdkReactNativeModule(reactContext: ReactApplicationContext) :
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun initialize(
+    organizationId: String,
+    settingsVersion: Int,
+    environment: String = "production",
+    promise: Promise
+  ) {
+    Flyreel.initialize(
+      reactApplicationContext.applicationContext as Application,
+      FlyreelConfiguration(
+        organizationId = organizationId,
+        settingsVersion = settingsVersion,
+        environment = mapEnvironment(environment)
+      )
+    )
+    promise.resolve(null)
   }
 
+  @ReactMethod
+  fun open(deeplinkUrl: String?, shouldSkipLoginPage: Boolean, promise: Promise) {
+    Flyreel.openFlyreel(
+      context = reactContext.currentActivity!!,
+      deeplinkUri = deeplinkUrl?.let { url -> Uri.parse(url) },
+      shouldSkipLoginPage = shouldSkipLoginPage
+    )
+    promise.resolve(null)
+  }
+
+  @ReactMethod
+  fun openWithCredentials(zipCode: String, accessCode: String, shouldSkipLoginPage: Boolean, promise: Promise) {
+    Flyreel.openFlyreel(
+      context = reactContext.currentActivity!!,
+      zipCode = zipCode,
+      accessCode = accessCode,
+      shouldSkipLoginPage = shouldSkipLoginPage
+    )
+    promise.resolve(null)
+  }
+
+  @ReactMethod
+  fun enableLogs(promise: Promise) {
+    Flyreel.enableLogs()
+    promise.resolve(null)
+  }
+
+  private fun mapEnvironment(environment: String) =
+    when (environment) {
+      "production" -> FlyreelEnvironment.Production
+      "sandbox" -> FlyreelEnvironment.Sandbox
+      else -> throw Exception("Wrong Flyreel environment")
+    }
+
   companion object {
-    const val NAME = "FlyreelSdkReactNative"
+    const val NAME = "Flyreel"
   }
 }
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,37 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'flyreel-sdk-react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  NativeModules,
+} from 'react-native';
+const { Flyreel } = NativeModules;
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    Flyreel.initialize('5d3633f9103a930011996475', 1, 'production');
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() =>
+          Flyreel.open(
+            'https://your.custom.url/?flyreelAccessCode=6M4T0T&flyreelZipCode=80212',
+            false
+          )
+        }
+      >
+        <Text style={styles.buttonText}>Open Flyreel</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Open Flyreel with URL</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Open Flyreel with credentials</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -27,5 +46,17 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    borderRadius: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });

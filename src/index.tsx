3ab0c4e523ebccf81eb0085c55305c090fcef494
AJ Patatanian <aj.patatanian@flyreel.co>
2024-05-06 14:09:55 -0500

    feat: added types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
 import { NativeModules, Platform } from 'react-native';
 
+type FlyreelSDKType = {
+  initialize(organizationId: String, settingsVersion: number): Promise<void>;
+  initializeWithSandbox(
+    organizationId: String,
+    settingsVersion: number
+  ): Promise<void>;
+  open(): Promise<void>;
+  openWithDeeplink(
+    deeplinkUrl: String,
+    shouldSkipLoginPage: Boolean
+  ): Promise<void>;
+  openWithCredentials(
+    zipCode: String,
+    accessCode: String,
+    shouldSkipLoginPage: boolean
+  ): Promise<void>;
+  enableLogs(): Promise<void>;
+};
+
 const LINKING_ERROR =
   `The package 'flyreel-sdk-react-native' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
@@ -17,4 +36,4 @@ const Flyreel = NativeModules.Flyreel
       }
     );
 
-export default Flyreel;
+export default Flyreel as FlyreelSDKType;

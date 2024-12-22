4e3193d9878d179bb2bb0c1c500a1d8c477e97dc
Paweł Ługowski <pawel.lugowski@crustlab.com>
2024-12-22 19:30:30 +0100

    feat: fixed ios API

--- ios/FlyreelSdkReactNative.swift
+++ ios/FlyreelSdkReactNative.swift
@@ -1,95 +1,224 @@
 @_spi(FlyreelInternal) import Flyreel
 
 @objc(FlyreelSdkReactNative)
-class FlyreelSdkReactNative: NSObject {
-    
-    @objc(initialize:settingsVersion:resolve:reject:)
-    func initialize(_ organizationId: String, settingsVersion: NSNumber, resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-        let configuration = FlyreelConfiguration(
-                        settingsVersion: settingsVersion.stringValue,
-                        organizationId: organizationId)
-        FlyreelSDK.shared.set(configuration: configuration)
-        resolve(nil)
-    }
+class FlyreelSdkReactNative: RCTEventEmitter {
 
-    @objc(initializeWithSandbox:settingsVersion:resolve:reject:)
-    func initializeWithSandbox(_ organizationId: String, settingsVersion: NSNumber, resolve: RCTPromiseResolveBlock,
-                    rejecter reject: RCTPromiseRejectBlock) -> Void {
-        let configuration = FlyreelConfiguration(
-                        settingsVersion: settingsVersion.stringValue,
-                        organizationId: organizationId,
-                        environment: FlyreelEnvironment.sandbox
-                    )
-        FlyreelSDK.shared.set(configuration: configuration)
-        resolve(nil)
-    }
+  @objc(initialize:settingsVersion:environment:resolve:reject:)
+  func initialize(
+    _ organizationId: String, settingsVersion: NSNumber, environment: String,
+    resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock
+  ) {
+    let configuration = FlyreelConfiguration(
+      settingsVersion: settingsVersion.stringValue,
+      organizationId: organizationId,
+      environment: mapEnvironment(environment: environment)
+    )
+    FlyreelSDK.shared.set(configuration: configuration)
+    resolve(nil)
+  }
 
-    @objc(initializeWithStaging:settingsVersion:resolve:reject:)
-    func initializeWithStaging(_ organizationId: String, settingsVersion: NSNumber, resolve: RCTPromiseResolveBlock,
-                    rejecter reject: RCTPromiseRejectBlock) -> Void {
-        let configuration = FlyreelConfiguration(
-                        settingsVersion: settingsVersion.stringValue,
-                        organizationId: organizationId,
-                        environment: FlyreelEnvironment.staging
-                    )
-        FlyreelSDK.shared.set(configuration: configuration)
-        resolve(nil)
-    }
-    
-    @objc(open:reject:)
-    func open(_ resolve: RCTPromiseResolveBlock,
-              rejecter reject: RCTPromiseRejectBlock) -> Void {
-        let rootView = UIApplication.shared.delegate!.window!!.rootViewController!
-        FlyreelSDK.shared.presentFlyreel(on: rootView)
-        resolve(nil)
-    }
+  @objc(open:reject:)
+  func open(
+    _ resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
+    let rootView = UIApplication.shared.delegate!.window!!.rootViewController!
+    FlyreelSDK.shared.presentFlyreel(on: rootView)
+    resolve(nil)
+  }
 
-    @objc(openWithDeeplink:shouldSkipLoginPage:resolve:reject:)
-    func openWithDeeplink(_ deeplink: String, shouldSkipLoginPage: Bool, resolve: RCTPromiseResolveBlock,
-              rejecter reject: RCTPromiseRejectBlock) -> Void {
+  @objc(openWithDeeplink:shouldSkipLoginPage:resolve:reject:)
+  func openWithDeeplink(
+    _ deeplink: String, shouldSkipLoginPage: Bool,
+    resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
 
-        let rootView = UIApplication.shared.delegate!.window!!.rootViewController!
-        if let deeplinkURL = URL(string: deeplink ?? "") {
-            FlyreelSDK.shared.presentFlyreel(on: rootView, deepLinkURL: deeplinkURL, shouldSkipLoginPage: shouldSkipLoginPage)
+    let rootView = UIApplication.shared.delegate!.window!!.rootViewController!
+    if let deeplinkURL = URL(string: deeplink ?? "") {
+      FlyreelSDK.shared.presentFlyreel(
+        on: rootView, deepLinkURL: deeplinkURL,
+        shouldSkipLoginPage: shouldSkipLoginPage)
+    } else {
+      FlyreelSDK.shared.presentFlyreel(on: rootView)
+    }
+    resolve(nil)
+  }
+
+  @objc(openWithCredentials:accessCode:shouldSkipLoginPage:resolve:reject:)
+  func openWithCredentials(
+    _ zipCode: String, accessCode: String, shouldSkipLoginPage: Bool,
+    resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
+    let rootView = UIApplication.shared.delegate!.window!!.rootViewController!
+    FlyreelSDK.shared.presentFlyreel(
+      on: rootView, zipCode: zipCode, accessCode: accessCode,
+      shouldSkipLoginPage: shouldSkipLoginPage)
+
+    resolve(nil)
+  }
+
+  @objc(checkStatus:accessCode:resolve:reject:)
+  func checkStatus(
+    _ zipCode: String, accessCode: String,
+    resolve: @escaping RCTPromiseResolveBlock,
+    rejecter reject: @escaping RCTPromiseRejectBlock
+  ) {
+    FlyreelSDK.shared.fetchFlyreelStatus(
+      zipCode: zipCode, accessCode: accessCode
+    ) { statusResult in
+      switch statusResult {
+      case .success(let flyreelStatus):
+        let result: NSMutableDictionary = [:]
+        result["status"] = flyreelStatus.status
+        result["expiration"] = flyreelStatus.expiration
+        resolve(result)
+      case .failure(let error):
+        if case FlyreelError.apiError(let message, let code) = error {
+          reject(String(code), message, nil)
         } else {
-            FlyreelSDK.shared.presentFlyreel(on: rootView)
+          reject("500", "Unexpected error", error)
         }
-        resolve(nil)
+      }
     }
-    
-    @objc(openWithCredentials:accessCode:shouldSkipLoginPage:resolve:reject:)
-    func openWithCredentials(_ zipCode: String, accessCode: String, shouldSkipLoginPage: Bool, resolve: RCTPromiseResolveBlock,
-                             rejecter reject: RCTPromiseRejectBlock) -> Void {
-        let rootView = UIApplication.shared.delegate!.window!!.rootViewController!
-        FlyreelSDK.shared.presentFlyreel(on: rootView, zipCode: zipCode, accessCode: accessCode, shouldSkipLoginPage: shouldSkipLoginPage)
-        
-        resolve(nil)
-    }
+  }
 
-    @objc(checkStatus:accessCode:resolve:reject:)
-    func checkStatus(_ zipCode: String, accessCode: String, resolve: @escaping RCTPromiseResolveBlock,
-                             rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
-        FlyreelSDK.shared.fetchFlyreelStatus(zipCode: zipCode, accessCode: accessCode) { statusResult in
-            switch statusResult {
-            case .success(let flyreelStatus):
-                let result: NSMutableDictionary = [:]
-                result["status"] = flyreelStatus.status
-                result["expiration"] = flyreelStatus.expiration
-                resolve(result)
-            case .failure(let error):
-                if case FlyreelError.apiError(let message, let code) = error {
-                    reject(String(code), message, nil)
-                } else {
-                    reject("500", "Unexpected error", error)
-                }
-            }
-        }
+  @objc(enableLogs:reject:)
+  func enableLogs(
+    _ resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
+    FlyreelSDK.shared.enableLogs()
+    resolve(nil)
+  }
+
+  func mapEnvironment(environment: String) -> FlyreelEnvironment {
+    switch environment {
+    case "production":
+      return .production
+    case "sandbox":
+      return .sandbox
+    case "staging":
+      return .staging
+    default:
+      return .production
     }
-    
-    @objc(enableLogs:reject:)
-    func enableLogs(_ resolve: RCTPromiseResolveBlock,
-                    rejecter reject: RCTPromiseRejectBlock) -> Void {
-        FlyreelSDK.shared.enableLogs()
-        resolve(nil)
+  }
+
+  // Supported events
+  override func supportedEvents() -> [String] {
+    return ["FlyreelAnalyticEvent"]
+  }
+
+  @objc(addAnalyticEventsListener:reject:)
+  func addAnalyticEventsListener(
+    _ resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
+    FlyreelSDK.shared.observeAnalyticEvents { event in
+      self.sendEvent(
+        withName: "FlyreelAnalyticEvent", body: event.toFlutterMap())
     }
+    resolve(nil)
+  }
+}
+
+extension FlyreelAnalyticEvent {
+  func toFlutterMap() -> [String: Any?] {
+    return [
+      "user": user?.toFlutterMap(),
+      "name": eventName,
+      "timestamp": ISO8601DateFormatter().string(from: timestamp),
+      "activeTime": activeTimeFromTrigger(),
+      "coordination": coordinationFromTrigger()?.toFlutterMap(),
+      "deviceData": deviceDataFromTrigger()?.toFlutterMap(),
+      "messageDetails": messageDetailsFromTrigger()?.toFlutterMap(),
+    ]
+  }
+
+  private func activeTimeFromTrigger() -> TimeInterval? {
+    switch trigger {
+    case .questionAsked(let activeTime, _),
+      .questionAnswered(let activeTime, _, _),
+      .flyreelCompleted(let activeTime):
+      return activeTime
+    case .userLoggedIn:
+      return nil
+    }
+  }
+
+  private func coordinationFromTrigger() -> FlyreelCoordinate? {
+    switch trigger {
+    case .questionAnswered(_, let location, _),
+      .userLoggedIn(_, _, let location, _):
+      return location
+    default:
+      return nil
+    }
+  }
+
+  private func deviceDataFromTrigger() -> FlyreelDeviceData? {
+    switch trigger {
+    case .userLoggedIn(_, let deviceData, _, _):
+      return deviceData
+    default:
+      return nil
+    }
+  }
+
+  private func messageDetailsFromTrigger() -> AnalyticMessageDetails? {
+    switch trigger {
+    case .questionAsked(_, let message),
+      .questionAnswered(_, _, let message):
+      return message
+    default:
+      return nil
+    }
+  }
+}
+
+extension FlyreelDeviceData {
+  func toFlutterMap() -> [String: String?] {
+    return [
+      "phoneModel": phoneModel,
+      "appVersion": appVersion,
+      "appName": appName,
+    ]
+  }
+}
+
+extension FlyreelCoordinate {
+  func toFlutterMap() -> [String: Any] {
+    return [
+      "lat": lat,
+      "lng": lng,
+    ]
+  }
+}
+
+extension AnalyticMessageDetails {
+  func toFlutterMap() -> [String: Any] {
+    return [
+      "message": message,
+      "messageType": messageType,
+      "moduleKey": moduleKey,
+      "messageKey": messageKey,
+      "phase": phase,
+    ]
+  }
+}
+
+extension FlyreelAnalyticUser {
+  func toFlutterMap() -> [String: Any?] {
+    return [
+      "id": flyreelID,
+      "name": name,
+      "email": email,
+      "botId": botId,
+      "botName": botName,
+      "organizationId": organizationId,
+      "status": status,
+    ]
+  }
 }
